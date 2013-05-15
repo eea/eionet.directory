78d279b233018c6ee9bf6cc128da756a269eafd2
roug <roug@26ecc896-f4e2-0310-9f2f-ca50803180eb>
2013-05-15 09:18:10 +0000

    Left-over README from before it was a Maven project

--- src/main/java/eionet/directory/package-info.java
+++ src/main/java/eionet/directory/package-info.java
@@ -1,5 +1,5 @@
 /**
- * The library contains lasses for making requests to Eionet User Directory.
+ * The library contains classes for making requests to the Eionet User Directory.
  * The methods allow to authenticate users and query information about LDAP users and roles.
  *
  * All requests should go through adapter class static methods <code>eionet.directory.DirectoryService</code>.
